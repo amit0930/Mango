1e151d3f166c585d5841bd221b73197d69d6c3c4
amit0930 <168823170+amit0930@users.noreply.github.com>
2024-05-15 15:05:39 +0530

    commiting latest changes

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -3,10 +3,10 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddOcelot();
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 builder.AddAppAuthentication();
 var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
-app.UseOcelot();
+app.UseOcelot().GetAwaiter().GetResult();
 app.Run();
